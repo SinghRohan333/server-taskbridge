b7085f5f94d16192f39f1cabcfb1c69a0717c0a5
SinghRohan333 <rohansingh12345602042@gmail.com>
2026-06-29 22:29:20 +0600

    ready for deployment push

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
     const db = client.db("taskbridge-db");
 
@@ -547,7 +547,7 @@ async function run() {
       });
     });
 
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!",
     );
